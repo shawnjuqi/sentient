4018bbc00ce4a7caa47ad9c780f3df0796eafe72
shawnjuqi <shawnickwang@gmail.com>
2025-12-10 16:06:28 -0500

    feat: app delegate

---
+++ sentient/AppDelegate.swift
@@ -0,0 +1,7 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+    }
+}
--- sentient/sentientApp.swift
+++ sentient/sentientApp.swift
@@ -4,10 +4,13 @@ import SwiftUI
 struct sentientApp: App {
     let persistenceController = PersistenceController.shared
 
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environment(\.managedObjectContext, 
+                    persistenceController.container.viewContext)
         }
     }
 }
